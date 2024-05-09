efe28fd74376495a1317f9292c4e924e2cfb4f74
Sam0042 <hisyammubarak90@gmail.com>
2024-05-09 10:00:46 +0700

    koreksi routes

--- covid-rest-api/routes/api.js
+++ covid-rest-api/routes/api.js
@@ -4,24 +4,23 @@ import validateTodo from "../middleware/validateTodo.js";
 import AuthController from "../controllers/AuthController.js";
 import auth from "../middleware/auth.js";
 
-
 const router = express.Router();
 
-router.get("/",function(req,res){
-    res.send("Halaman Home by Sam0042 and Fredy Rambitan Simanungkalit")
+router.get("/", function (req, res) {
+  res.send("Halaman Home by Sam0042 and Fredy Rambitan Simanungkalit");
 });
 
 const PatientsController = object;
 
 //PatientsController.method ditaruh di tempat callbackfunct  karena callbackfunct auto dijalankan ketika kita mengakses parameter satu
-router.get("/students",auth,PatientsController.index);
-router.post("/students",auth,validateTodo,PatientsController.store);
-// router.post("/students",auth,PatientsController.store);
-router.put("/students/:id",auth,PatientsController.update);
-router.delete("/students/:id",auth,PatientsController.destroy);
-router.get("/students/:id",auth,PatientsController.show);
+router.get("/patients", auth, PatientsController.index);
+router.post("/patients", auth, validateTodo, PatientsController.store);
+// router.post("/patients",auth,PatientsController.store);
+router.put("/patients/:id", auth, PatientsController.update);
+router.delete("/patients/:id", auth, PatientsController.destroy);
+router.get("/patients/:id", auth, PatientsController.show);
 //register-login
-router.post("/register",AuthController.register);
-router.post("/login",AuthController.login);
+router.post("/register", AuthController.register);
+router.post("/login", AuthController.login);
 
 export default router;
